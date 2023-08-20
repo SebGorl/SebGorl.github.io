9474641447e6f80bf2956c8b2e0da3deb27c6e3b
Sebastian Gorlak <gorl.seb64@gmail.com>
2023-08-20 15:57:48 +0200

    slider added, headlines fixed

---
+++ scripts/slider_use.js
@@ -0,0 +1,44 @@
+const slider = document.getElementById("slider");
+
+            const slide = document.querySelector(".slide");
+
+            var index = 0;
+
+            const prev = document.getElementById("previous-slide");
+            const next = document.getElementById("next-slide");
+
+            next.onclick = function()
+            {
+                var width = slide.clientWidth;
+
+                switch(index)
+                {
+                    case 2:
+                        slider.scrollLeft -= 3 * width;
+                        index = 0;
+                        break;
+
+                    default:
+                        slider.scrollLeft += width;
+                        index += 1;
+                        break;
+                }
+            };
+
+            prev.onclick = function()
+            {
+                var width = slide.clientWidth;
+
+                switch(index)
+                {
+                    case 0:
+                        slider.scrollLeft += 3 * width;
+                        index = 2;
+                        break;
+
+                    default:
+                        slider.scrollLeft -= width;
+                        index -= 1;
+                        break;
+                }
+            };
